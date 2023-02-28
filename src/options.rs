b05b5a3390567c3daf62053f58093409cb0785cc
Andrew Rondeau <fhjraklfhdsajklfadsjkl@fdsjkfndsajkfndasjkln.fds>
2023-02-27 21:32:19 -0500

    First pass at parsing options

--- src/options.rs
+++ src/options.rs
@@ -4,6 +4,16 @@ use std::path::Path;
 pub struct Options {
     pub source_wav_path: Box<Path>,
     pub target_wav_path: Box<Path>,
+    pub channels: Channels,
+    pub transform_mono: bool,
+    pub generate_center_channel: bool,
+    pub generate_subwoofer_channel: bool,
+}
+
+pub enum Channels {
+    Four,
+    Five,
+    FiveOne
 }
 
 impl Options {
@@ -26,13 +36,74 @@ impl Options {
         let target_wav_path = args_iter.next().unwrap();
         let target_wav_path = Path::new(target_wav_path.as_str());
 
+        let mut channels = Channels::FiveOne;
+
         // Iterate through the options
         // -channels
         // 4 or 5 or 5.1
 
-        Some(Options {
-            source_wav_path: source_wav_path.into(),
-            target_wav_path: target_wav_path.into(),
-        })
+        loop {
+            match args_iter.next() {
+                Some(flag) => {
+                    // Parse a flag
+                    if flag.eq("-channels") {
+                        match args_iter.next() {
+                            Some(channels_string) => {
+                                if channels_string.eq("4") {
+                                    channels = Channels::Four
+                                } else if channels_string.eq("5") {
+                                    channels = Channels::Five
+                                } else if channels_string.eq("5.1") {
+                                    channels = Channels::FiveOne
+                                } else {
+                                    println!("Unknown channel configuration: {}", channels_string);
+                                    return None;
+                                }
+                            },
+                            None => {
+                                println!("Channels unspecified");
+                                return None;
+                            }
+                        }
+                    } else {
+                        println!("Unknown flag: {}", flag);
+                        return None;
+                    }
+                },
+                None => {
+                    // No more flags left, interpret the options and return them
+                    let transform_mono: bool;
+                    let generate_center_channel: bool;
+                    let generate_subwoofer_channel: bool;
+                
+                    match channels {
+                        Channels::Four => {
+                            transform_mono = false;
+                            generate_center_channel = false;
+                            generate_subwoofer_channel = false
+                        },
+                        Channels::Five => {
+                            transform_mono = true;
+                            generate_center_channel = true;
+                            generate_subwoofer_channel = false
+                        },
+                        Channels::FiveOne => {
+                            transform_mono = true;
+                            generate_center_channel = true;
+                            generate_subwoofer_channel = true
+                        },
+                    }
+
+                    return Some(Options {
+                        source_wav_path: source_wav_path.into(),
+                        target_wav_path: target_wav_path.into(),
+                        channels,
+                        transform_mono,
+                        generate_center_channel,
+                        generate_subwoofer_channel
+                    });
+                }
+            }
+        }
     }
 }
