6a68e3832f31cdf2db70522febc2464fb80c131f
Andrew Rondeau <fhjraklfhdsajklfadsjkl@fdsjkfndsajkfndasjkln.fds>
2023-08-27 18:30:57 -0400

    Adjusted simulation results for phase difference

--- src/matrix.rs
+++ src/matrix.rs
@@ -250,49 +250,72 @@ impl Matrix for SQMatrix {
         right_amplitude: f32,
         right_phase: f32,
     ) -> FrequencyPans {
+        /*
 
-/*
+        right front to right rear
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.1; phase_difference: -0.069886, amplitude: 1.0724471
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.2; phase_difference: -0.13909595, amplitude: 1.1497524
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.3; phase_difference: -0.20699221, amplitude: 1.2318121
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.4; phase_difference: -0.2730087, amplitude: 1.3184603
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.5; phase_difference: -0.3366748, amplitude: 1.409481
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.6; phase_difference: -0.397628, amplitude: 1.5046198
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.7; phase_difference: -0.45561564, amplitude: 1.6035978
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.8; phase_difference: -0.51048833, amplitude: 1.7061238
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 0.9; phase_difference: -0.5621867, amplitude: 1.8119053
+        left_front: 0; right_front: 1; left_rear: 0; right_rear: 1; phase_difference: -0.610726, amplitude: 1.9206555
+        left_front: 0; right_front: 0.9; left_rear: 0; right_rear: 1; phase_difference: -0.6610432, amplitude: 1.8401754
+        left_front: 0; right_front: 0.8; left_rear: 0; right_rear: 1; phase_difference: -0.71883, amplitude: 1.7630146
+        left_front: 0; right_front: 0.7; left_rear: 0; right_rear: 1; phase_difference: -0.7853982, amplitude: 1.6899494
+        left_front: 0; right_front: 0.6; left_rear: 0; right_rear: 1; phase_difference: -0.8621701, amplitude: 1.6219544
+        left_front: 0; right_front: 0.5; left_rear: 0; right_rear: 1; phase_difference: -0.95054686, amplitude: 1.5602324
+        left_front: 0; right_front: 0.4; left_rear: 0; right_rear: 1; phase_difference: -1.0516503, amplitude: 1.5062258
+        left_front: 0; right_front: 0.3; left_rear: 0; right_rear: 1; phase_difference: -1.1659045, amplitude: 1.4615773
+        left_front: 0; right_front: 0.2; left_rear: 0; right_rear: 1; phase_difference: -1.2924967, amplitude: 1.428011
+        left_front: 0; right_front: 0.1; left_rear: 0; right_rear: 1; phase_difference: -1.4288993, amplitude: 1.4071068
 
-right front to right rear
-right_rear_amplitude: 0, right_front_amplitude: 1; phase_difference: 0, amplitude: 1
-right_rear_amplitude: 0.1, right_front_amplitude: 0.9; phase_difference: -0.07762151, amplitude: 0.97271806
-right_rear_amplitude: 0.2, right_front_amplitude: 0.8; phase_difference: -0.17324567, amplitude: 0.9521576
-right_rear_amplitude: 0.3, right_front_amplitude: 0.7; phase_difference: -0.29145682, amplitude: 0.9408214
-right_rear_amplitude: 0.4, right_front_amplitude: 0.6; phase_difference: -0.43662715, amplitude: 0.9421178
-right_rear_amplitude: 0.5, right_front_amplitude: 0.5; phase_difference: -0.610726, amplitude: 0.96032774
-right_rear_amplitude: 0.6, right_front_amplitude: 0.4; phase_difference: -0.80978364, amplitude: 1
-right_rear_amplitude: 0.7, right_front_amplitude: 0.3; phase_difference: -1.0214219, amplitude: 1.0645432
-right_rear_amplitude: 0.8, right_front_amplitude: 0.2; phase_difference: -1.2277725, amplitude: 1.1546428
-right_rear_amplitude: 0.9, right_front_amplitude: 0.1; phase_difference: -1.4133795, amplitude: 1.2678871
-right_rear_amplitude: 1, right_front_amplitude: 0; phase_difference: -1.5707964, amplitude: 1.4
+        right rear to left rear
+        left_front: 0; right_front: 0; left_rear: 0.1; right_rear: 1; phase_difference: -1.7701336, amplitude: 1.4069825
+        left_front: 0; right_front: 0; left_rear: 0.2; right_rear: 1; phase_difference: -1.9655875, amplitude: 1.4277254
+        left_front: 0; right_front: 0; left_rear: 0.3; right_rear: 1; phase_difference: -2.15371, amplitude: 1.4616429
+        left_front: 0; right_front: 0; left_rear: 0.4; right_rear: 1; phase_difference: -2.331809, amplitude: 1.5078461
+        left_front: 0; right_front: 0; left_rear: 0.5; right_rear: 1; phase_difference: -2.4980917, amplitude: 1.5652475
+        left_front: 0; right_front: 0; left_rear: 0.6; right_rear: 1; phase_difference: -2.6516354, amplitude: 1.6326665
+        left_front: 0; right_front: 0; left_rear: 0.7; right_rear: 1; phase_difference: -2.7922482, amplitude: 1.7089177
+        left_front: 0; right_front: 0; left_rear: 0.8; right_rear: 1; phase_difference: -2.9202783, amplitude: 1.7928748
+        left_front: 0; right_front: 0; left_rear: 0.9; right_rear: 1; phase_difference: -3.0364265, amplitude: 1.8835074
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 1; phase_difference: -3.1415927, amplitude: 1.9798989
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.9; phase_difference: 3.0364265, amplitude: 1.8835073
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.8; phase_difference: 2.9202783, amplitude: 1.7928747
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.7; phase_difference: 2.7922482, amplitude: 1.7089176
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.6; phase_difference: 2.6516356, amplitude: 1.6326665
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.5; phase_difference: 2.4980917, amplitude: 1.5652475
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.4; phase_difference: 2.3318093, amplitude: 1.5078461
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.3; phase_difference: 2.15371, amplitude: 1.4616429
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.2; phase_difference: 1.9655876, amplitude: 1.4277254
+        left_front: 0; right_front: 0; left_rear: 1; right_rear: 0.1; phase_difference: 1.7701335, amplitude: 1.4069825
 
-right rear to left rear
-left_amplitude: 0, right_aplitude: 1; phase_difference: -1.5707964, amplitude: 1.4
-left_amplitude: 0.1, right_aplitude: 0.9; phase_difference: -1.7921108, amplitude: 1.267754
-left_amplitude: 0.2, right_aplitude: 0.8; phase_difference: -2.0607538, amplitude: 1.1544696
-left_amplitude: 0.3, right_aplitude: 0.7; phase_difference: -2.38058, amplitude: 1.0662081
-left_amplitude: 0.4, right_aplitude: 0.6; phase_difference: -2.7468014, amplitude: 1.0095544
-left_amplitude: 0.5, right_aplitude: 0.5; phase_difference: -3.1415927, amplitude: 0.98994946
-left_amplitude: 0.6, right_aplitude: 0.4; phase_difference: 2.7468016, amplitude: 1.0095544
-left_amplitude: 0.7, right_aplitude: 0.3; phase_difference: 2.38058, amplitude: 1.0662081
-left_amplitude: 0.8, right_aplitude: 0.2; phase_difference: 2.0607538, amplitude: 1.1544696
-left_amplitude: 0.9, right_aplitude: 0.1; phase_difference: 1.7921109, amplitude: 1.2677538
-left_amplitude: 1, right_aplitude: 0; phase_difference: 1.5707961, amplitude: 1.4
+        left front to left rear
 
-left front to left rear
-left_rear_amplitude: 1, left_front_amplitude: 0; phase_difference: 1.5707961, amplitude: 1.4
-left_rear_amplitude: 0.9, left_front_amplitude: 0.1; phase_difference: 1.728213, amplitude: 1.2678871
-left_rear_amplitude: 0.8, left_front_amplitude: 0.2; phase_difference: 1.91382, amplitude: 1.1546428
-left_rear_amplitude: 0.7, left_front_amplitude: 0.3; phase_difference: 2.1201706, amplitude: 1.0645432
-left_rear_amplitude: 0.6, left_front_amplitude: 0.4; phase_difference: 2.3318088, amplitude: 1
-left_rear_amplitude: 0.5, left_front_amplitude: 0.5; phase_difference: 2.5308666, amplitude: 0.96032774
-left_rear_amplitude: 0.4, left_front_amplitude: 0.6; phase_difference: 2.7049654, amplitude: 0.9421178
-left_rear_amplitude: 0.3, left_front_amplitude: 0.7; phase_difference: 2.8501358, amplitude: 0.9408214
-left_rear_amplitude: 0.2, left_front_amplitude: 0.8; phase_difference: 2.9683468, amplitude: 0.9521576
-left_rear_amplitude: 0.1, left_front_amplitude: 0.9; phase_difference: 3.063971, amplitude: 0.97271806
-left_rear_amplitude: 0, left_front_amplitude: 1; phase_difference: 3.1415925, amplitude: 1
-
-*/
+        left_front: 0.1; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 1.7126932, amplitude: 1.4071068
+        left_front: 0.2; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 1.8490958, amplitude: 1.428011
+        left_front: 0.3; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 1.975688, amplitude: 1.4615773
+        left_front: 0.4; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.0899422, amplitude: 1.5062258
+        left_front: 0.5; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.1910458, amplitude: 1.5602324
+        left_front: 0.6; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.2794223, amplitude: 1.6219544
+        left_front: 0.7; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.3561943, amplitude: 1.6899494
+        left_front: 0.8; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.4227624, amplitude: 1.7630146
+        left_front: 0.9; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.4805493, amplitude: 1.8401754
+        left_front: 1; right_front: 0; left_rear: 1; right_rear: 0; phase_difference: 2.5308666, amplitude: 1.9206555
+        left_front: 1; right_front: 0; left_rear: 0.1; right_rear: 0; phase_difference: 3.0717065, amplitude: 1.0724471
+        left_front: 1; right_front: 0; left_rear: 0.2; right_rear: 0; phase_difference: 3.0024965, amplitude: 1.1497524
+        left_front: 1; right_front: 0; left_rear: 0.3; right_rear: 0; phase_difference: 2.9346004, amplitude: 1.2318121
+        left_front: 1; right_front: 0; left_rear: 0.4; right_rear: 0; phase_difference: 2.8685837, amplitude: 1.3184603
+        left_front: 1; right_front: 0; left_rear: 0.5; right_rear: 0; phase_difference: 2.8049178, amplitude: 1.409481
+        left_front: 1; right_front: 0; left_rear: 0.6; right_rear: 0; phase_difference: 2.7439644, amplitude: 1.5046198
+        left_front: 1; right_front: 0; left_rear: 0.7; right_rear: 0; phase_difference: 2.685977, amplitude: 1.6035978
+        left_front: 1; right_front: 0; left_rear: 0.8; right_rear: 0; phase_difference: 2.6311042, amplitude: 1.7061238
+        left_front: 1; right_front: 0; left_rear: 0.9; right_rear: 0; phase_difference: 2.5794058, amplitude: 1.8119053
+        */
 
         let amplitude_sum = left_amplitude + right_amplitude;
         let left_to_right = (left_amplitude / amplitude_sum) * 2.0 - 1.0;
@@ -309,18 +332,18 @@ left_rear_amplitude: 0, left_front_amplitude: 1; phase_difference: 3.1415925, am
             // Tone is 100% in the front
             return FrequencyPans {
                 left_to_right,
-                back_to_front: 0.0
+                back_to_front: 0.0,
             };
         } else if phase_difference <= HALF_NPI {
             // Tone is 100% in the rear, steered to the right by phase
             /*
-left_amplitude: 0, right_aplitude: 1; phase_difference: -1.5707964, amplitude: 1.4
-left_amplitude: 0.1, right_aplitude: 0.9; phase_difference: -1.7921108, amplitude: 1.267754
-left_amplitude: 0.2, right_aplitude: 0.8; phase_difference: -2.0607538, amplitude: 1.1544696
-left_amplitude: 0.3, right_aplitude: 0.7; phase_difference: -2.38058, amplitude: 1.0662081
-left_amplitude: 0.4, right_aplitude: 0.6; phase_difference: -2.7468014, amplitude: 1.0095544
-left_amplitude: 0.5, right_aplitude: 0.5; phase_difference: -3.1415927, amplitude: 0.98994946
-            */
+            left_amplitude: 0, right_aplitude: 1; phase_difference: -1.5707964, amplitude: 1.4
+            left_amplitude: 0.1, right_aplitude: 0.9; phase_difference: -1.7921108, amplitude: 1.267754
+            left_amplitude: 0.2, right_aplitude: 0.8; phase_difference: -2.0607538, amplitude: 1.1544696
+            left_amplitude: 0.3, right_aplitude: 0.7; phase_difference: -2.38058, amplitude: 1.0662081
+            left_amplitude: 0.4, right_aplitude: 0.6; phase_difference: -2.7468014, amplitude: 1.0095544
+            left_amplitude: 0.5, right_aplitude: 0.5; phase_difference: -3.1415927, amplitude: 0.98994946
+                        */
 
             // -1.5707964 -> 1
             // -PI -> 0
@@ -331,17 +354,17 @@ left_amplitude: 0.5, right_aplitude: 0.5; phase_difference: -3.1415927, amplitud
             return FrequencyPans {
                 // Right to left panning: -1 is left, 1 is right
                 left_to_right: (HALF_PI - ppd_zeroed) / HALF_PI,
-                back_to_front: 1.0
+                back_to_front: 1.0,
             };
         } else if phase_difference >= HALF_PI {
             // Tone is 100% in the rear, steered to the left by phase
             /*
-left_amplitude: 0.6, right_aplitude: 0.4; phase_difference: 2.7468016, amplitude: 1.0095544
-left_amplitude: 0.7, right_aplitude: 0.3; phase_difference: 2.38058, amplitude: 1.0662081
-left_amplitude: 0.8, right_aplitude: 0.2; phase_difference: 2.0607538, amplitude: 1.1544696
-left_amplitude: 0.9, right_aplitude: 0.1; phase_difference: 1.7921109, amplitude: 1.2677538
-left_amplitude: 1, right_aplitude: 0; phase_difference: 1.5707961, amplitude: 1.4
-            */
+            left_amplitude: 0.6, right_aplitude: 0.4; phase_difference: 2.7468016, amplitude: 1.0095544
+            left_amplitude: 0.7, right_aplitude: 0.3; phase_difference: 2.38058, amplitude: 1.0662081
+            left_amplitude: 0.8, right_aplitude: 0.2; phase_difference: 2.0607538, amplitude: 1.1544696
+            left_amplitude: 0.9, right_aplitude: 0.1; phase_difference: 1.7921109, amplitude: 1.2677538
+            left_amplitude: 1, right_aplitude: 0; phase_difference: 1.5707961, amplitude: 1.4
+                        */
 
             // 1.5707964 -> -1
             // PI -> 0
@@ -352,7 +375,7 @@ left_amplitude: 1, right_aplitude: 0; phase_difference: 1.5707961, amplitude: 1.
             return FrequencyPans {
                 // Right to left panning: -1 is left, 1 is right
                 left_to_right: right_to_left * -1.0,
-                back_to_front: 1.0
+                back_to_front: 1.0,
             };
         }
 
